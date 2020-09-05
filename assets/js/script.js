3d7c27b611a1c324634f2873edc8b85e9188880c
daejo <daejo@icloud.com>
2020-09-04 19:39:16 -0700

    tests for loop for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,124 +9,118 @@
     A modal where I can input a different city to search for will pop up and you can do the process over again.
 */
 
-var searchButton = document.querySelector(".searchButton");
-var inputValue = docuent.querySelector(".inputValue");
+// var searchButton = document.querySelector(".searchButton");
+// var inputValue = docuent.querySelector(".inputValue");
 
-// storage array
-var citySearched = [];
+// // storage array
+// var citySearched = [];
 
-// local storage
-
-
-localStorage.setItem();
-localStorage.getItem();
+// // local storage
 
 
+// localStorage.setItem();
+// localStorage.getItem();
 
 
-// dashboard - Weather 5-Day forecast - search by city
-var cityWeather = function(value) {
-
-    // format api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+value+"&units=imperial&appid=386eafe2ba649945a853251bb7d3f25e";
-
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-
-            // loop from day one to day 6
-            for (var i = 1; i <6; i++) {
-
-                // query selector variables
-                var dateEl = document.querySelector("#day" + i);
-                var tempEl = document.querySelector("#temp" + i);
-                var descEl = document.querySelector("#desc" + i);
-                var iconEl = document.querySelector("#icon" + i);
-
-                // equation variables
-                var date = moment().add.(i, 'days').format('l');
-                dateEl.innerHTML = date;
-
-                var temperatureValue = data.list[i].main.temp;
-                var fixTemp = temperatureValue.toFixed(1);
-                tempEl.innerHTML = fixTemp + "&deg; F";
-
-                var descriptionValue = data.list[i].weather.description;
-                descEl.innerHTML = descriptionValue;
-
-                var iconVal = data.list[i].weather[0].icon;
-                iconEl.setAttribute("src", "https://openweathermap.org/img/w/" + iconVal + ".png");
-
-            }
-            })
-        }
-    })
-}
-
-// api key a9kdLi-v4txmsrRgTFeTgvVop5k6ldC0K1quPvEG7qqeKZSbsuXE0Ta6CdmOkY1gsbfOg6tGNlj8_0fkj2BKjteccbnYUXkrP59niNbGJUv5YpU7TyngFlTEgwRPX3Yx
-// dashboard - Yelp Food - search by city
-var cityFood = function (value) {
 
 
-}
+// // dashboard - Weather 5-Day forecast - search by city
+// var cityWeather = function(value) {
 
-// when search button is clicked
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+//     // format api url
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+value+"&units=imperial&appid=386eafe2ba649945a853251bb7d3f25e";
 
-    var cityInput = inputValue.value.trim();
-    if (cityInput) {
+//     // make a request to the url
+//     fetch(apiUrl).then(function(response) {
+
+//         // request was successful
+//         if (response.ok) {
+//             response.json().then(function(data) {
+
+//             // loop from day one to day 6
+//             for (var i = 1; i <6; i++) {
+
+//                 // query selector variables
+//                 var dateEl = document.querySelector("#day" + i);
+//                 var tempEl = document.querySelector("#temp" + i);
+//                 var descEl = document.querySelector("#desc" + i);
+//                 var iconEl = document.querySelector("#icon" + i);
+
+//                 // equation variables
+//                 var date = moment().add.(i, 'days').format('l');
+//                 dateEl.innerHTML = date;
+
+//                 var temperatureValue = data.list[i].main.temp;
+//                 var fixTemp = temperatureValue.toFixed(1);
+//                 tempEl.innerHTML = fixTemp + "&deg; F";
+
+//                 var descriptionValue = data.list[i].weather.description;
+//                 descEl.innerHTML = descriptionValue;
+
+//                 var iconVal = data.list[i].weather[0].icon;
+//                 iconEl.setAttribute("src", "https://openweathermap.org/img/w/" + iconVal + ".png");
+
+//             }
+//             })
+//         }
+//     })
+// }
+
+// // api key a9kdLi-v4txmsrRgTFeTgvVop5k6ldC0K1quPvEG7qqeKZSbsuXE0Ta6CdmOkY1gsbfOg6tGNlj8_0fkj2BKjteccbnYUXkrP59niNbGJUv5YpU7TyngFlTEgwRPX3Yx
+// // dashboard - Yelp Food - search by city
+// var cityFood = function (value) {
+
+
+// }
+
+// // when search button is clicked
+// var formSubmitHandler = function(event) {
+//     event.preventDefault();
+
+//     var cityInput = inputValue.value.trim();
+//     if (cityInput) {
         
-        localStorage.setItem("", JSON.stringify(citySearched));
-    }
-    cityWeather(cityInput);
-}
+//         localStorage.setItem("", JSON.stringify(citySearched));
+//     }
+//     cityWeather(cityInput);
+// }
 
-var cityWeather = function(value) {
-
-    // format api url
-    var hotelInput = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=1500&type=hotel&keyword=hotel&key=AIzaSyA8cerxaXUNfNgMNLFXuh4NPEkM5i7mLXc";
-    var locationKey = "location=-33.8670522,151.1957362";
-    var hotelUrl = hotelInput + locationKey;
-
-    // make a request to the url
+var hotelSearch = function() {
+    var hotelCoord = long + lat;
+    var hotelInput = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=hotel&keyword=hotel&key=AIzaSyA8cerxaXUNfNgMNLFXuh4NPEkM5i7mLXc";
+    var corsInput = "https://cors-anywhere.herokuapp.com/" //Fixes "cors" error.
+    var hotelUrl = corsInput + hotelInput
+    
+    
     fetch(hotelUrl).then(function(response) {
+        return response.json();
+        })
+        .then(function(response) { 
+            console.log(response.results[0].name);
+            
+             for (var i = 0; i < 5; i++) {
 
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-
-            // loop from day one to day 6
-            for (var i = 1; i <6; i++) {
-
-                // query selector variables
-                var dateEl = document.querySelector("#day" + i);
-                var tempEl = document.querySelector("#temp" + i);
-                var descEl = document.querySelector("#desc" + i);
+                // Links elements to document ids
+                var hotelNameEl = document.querySelector("#hotel-name" + i);
+                var hotelAddressEl = document.querySelector("#hotel-address" + i);
+                var hotelRatingEl = document.querySelector("#hotel-rating" + i);
                 var iconEl = document.querySelector("#icon" + i);
 
-                // equation variables
-                var date = moment().add.(i, 'days').format('l');
-                dateEl.innerHTML = date;
+                var hotelName = response.results[i].name;
+                hotelNameEl.innerText = hotelName;
 
-                var temperatureValue = data.list[i].main.temp;
-                var fixTemp = temperatureValue.toFixed(1);
-                tempEl.innerHTML = fixTemp + "&deg; F";
+                var hotelAddress = response.results[i].vicinity;
+                hotelAddressEl.innerText = hotelAddress;
 
-                var descriptionValue = data.list[i].weather.description;
-                descEl.innerHTML = descriptionValue;
+                var hotelRating = response.results[i].rating;
+                hotelRatingEl.innerText = hotelRating;
 
                 var iconVal = data.list[i].weather[0].icon;
                 iconEl.setAttribute("src", "https://openweathermap.org/img/w/" + iconVal + ".png");
 
             }
-            })
-        }
-    })
+        });  
 }
 
 
-searchButton.addEventListener("click", formSubmitHandler);
+// searchButton.addEventListener("click", formSubmitHandler);
